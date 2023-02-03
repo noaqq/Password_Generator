897bd1a2cf9af22e72895b97c088745a1e1d4b19
noaqq <maximbaymetov2002@icloud.com>
2023-02-03 20:06:47 +0300

    add bootstrap5 and final fix

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -6,10 +6,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'generator/home.html')
-
-def about(request):
-    return render(request, 'generator/about.html')
-
+    
 
 def password(request):
     thepassword = ''
