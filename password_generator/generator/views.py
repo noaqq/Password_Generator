030609d589575ef317056e28fad384fe0e90d2f4
noaqq <maximbaymetov2002@icloud.com>
2023-02-03 19:48:12 +0300

    add about page

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -7,13 +7,24 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'generator/home.html')
 
+def about(request):
+    return render(request, 'generator/about.html')
+
 
 def password(request):
     thepassword = ''
-    
+
     charcaters = list('abcdefghijklmnopqrstuvwxyz')
 
-    lenght = 10
+    if request.GET.get("uppercase"):
+        charcaters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get("special"):
+        charcaters.extend(list('!@#$%^&*()_+-='))
+    if request.GET.get("numbers"):
+        charcaters.extend(list('1234567890'))
+
+    lenght = int(request.GET.get("length", 12))
+
     for x in range(lenght):
         thepassword += random.choice(charcaters)
     return render(request, 'generator/password.html', {"password":thepassword})
