7d7b3022614a1fcf4eae075bbc4f06e6fec95274
noaqq <maximbaymetov2002@icloud.com>
2023-02-03 19:13:33 +0300

    generate function

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -1,3 +1,20 @@
+import random
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'generator/home.html')
+
+
+def password(request):
+    thepassword = ''
+    
+    charcaters = list('abcdefghijklmnopqrstuvwxyz')
+
+    lenght = 10
+    for x in range(lenght):
+        thepassword += random.choice(charcaters)
+    return render(request, 'generator/password.html', {"password":thepassword})
+
